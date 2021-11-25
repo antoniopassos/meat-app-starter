bae798629ce5ddd97afb318dba8183704b10a7bb
Antonio Passos <antoniopassos@gmail.com>
2021-11-25 14:31:51 -0300

    Implemented lesson 79-Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,14 @@ import { HomeComponent } from './home/home.component';
 import { ROUTES } from './app.routes';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { RestauranteComponent } from './restaurantes/restaurante/restaurante.component';
-import { RestauranteService } from './restaurantes/restaurantes.service';
 import { RestauranteDetalheComponent } from './restaurante-detalhe/restaurante-detalhe.component';
 import { MenuComponent } from './restaurante-detalhe/menu/menu.component';
 import { ShoppingCartComponent } from './restaurante-detalhe/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurante-detalhe/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurante-detalhe/reviews/reviews.component';
-import { ShoppingCartService } from './restaurante-detalhe/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,9 +36,10 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpModule,
     SharedModule,
+    CoreModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestauranteService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
